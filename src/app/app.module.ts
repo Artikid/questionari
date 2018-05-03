bd321b7f89f8a61a1c545ef47dce9d23ec86df08
Andrea Cammarata <andrea.cammarata@mminformatica.it>
2018-05-03 17:48:01 +0200

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
